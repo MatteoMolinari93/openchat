9a06285a3d4c5647846d5820e6f256fe6eb4f2af
Molinari <matteomolinari93@gmail.com>
2020-10-20 16:01:50 +0200

    Added Spring REST Docs

--- src/test/java/com/molim/cleancoders/openchat/web/controllers/RegistrationControllerTest.java
+++ src/test/java/com/molim/cleancoders/openchat/web/controllers/RegistrationControllerTest.java
@@ -1,13 +1,17 @@
 package com.molim.cleancoders.openchat.web.controllers;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.hateoas.MediaTypes;
 import org.springframework.http.MediaType;
+import org.springframework.restdocs.RestDocumentationExtension;
+import org.springframework.restdocs.payload.RequestFieldsSnippet;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -16,12 +20,14 @@ import com.molim.cleancoders.openchat.web.models.UserDto;
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-import org.apache.catalina.User;
-
 import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.*;
 import static org.hamcrest.Matchers.*;
 import static org.mockito.Mockito.*;
+import static org.springframework.restdocs.payload.PayloadDocumentation.*;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.*;
 
+@ExtendWith(RestDocumentationExtension.class)
+@AutoConfigureRestDocs(uriScheme = "https", uriHost = "dev.springframework.guru", uriPort = 80)
 @TestPropertySource("classpath:application.properties")
 @WebMvcTest(RegistrationController.class)
 public class RegistrationControllerTest {
@@ -50,6 +56,18 @@ public class RegistrationControllerTest {
 				+ "	\"about\" : \"I love playing the piano and travelling.\"\r\n"
 				+ "}\r\n"
 				+ "").contentType(MediaType.APPLICATION_JSON))
+			.andDo(document("/registration",
+					requestFields(
+							fieldWithPath("username").description("New User Username"),
+							fieldWithPath("password").description("New User Password"),
+							fieldWithPath("about").description("General information about the user.")
+							),
+					responseFields(
+							fieldWithPath("id").description("New User Id"),
+							fieldWithPath("username").description("New User Username"),
+							fieldWithPath("password").ignored(),
+							fieldWithPath("about").description("General information about the user.")
+							)))
 			.andExpect(status().isCreated())
 			.andExpect(content().contentType(MediaTypes.HAL_JSON))
 			.andExpect(jsonPath("$.username", is("Alice")))
