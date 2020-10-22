20412e2444a0f6cc282ecdec0ef598971b0d8129
Molinari <matteomolinari93@gmail.com>
2020-10-22 11:27:48 +0200

    PostController Documentation

--- src/test/java/com/molim/cleancoders/openchat/services/PostServiceTest.java
+++ src/test/java/com/molim/cleancoders/openchat/services/PostServiceTest.java
@@ -7,7 +7,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Optional;
 
